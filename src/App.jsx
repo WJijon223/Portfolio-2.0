9f2bcdaea78d89ff08211c30705404273c58cafb
WJijon223 <williamjijon223@gmail.com>
2025-08-21 01:19:20 -0400

    made the mobile view for the skills component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
