baeab03358ef7dc39e8a7e64259a8186b07a1100
WJijon223 <williamjijon223@gmail.com>
2025-08-21 23:51:24 -0400

    changed skills logos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
